b73a7f7e04160d2642387c230591623d298449a9
Harish Garg <rishusharma123321@gmail.com>
2025-04-08 23:19:39 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import path from "path";
+import cookieParser from "cookie-parser";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -8,30 +9,27 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 
-
-import cookieParser from "cookie-parser";
-
-
 dotenv.config();
+
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
-
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 
-
-// app.get("/", (req, res) => {
-// 	res.send("Hello World !");
-// });
-
-
-
+app.use((req, res, next) => {
+	if (req.path.startsWith('/api/')) {
+	  return next();
+	}
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
 
 server.listen(PORT, () => {
 	connectToMongoDB();
